a9452a7297ba5d4e1439273de43ba47dec7b188d
Jeremy Richards <jeremyrichards2289@gmail.com>
2020-08-13 18:48:13 +0200

    stupicC2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,17 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:name]
-
-  end
-end
-
-  if question == "i am going to work right now!"
-    ""
-    puts answer == "shupup"
-  elsif question.ends_with?("?")
-    "Silly question, get dressed and go to work!"
-  else
-    "I don't care, get dressed and go to work!"
+    @question = params[:question]
+      if @question == "I am going to work"
+        @answer = "Great!"
+      elsif @question.include? "?"
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don´t care, get dressed and go to work!"
+      end
   end
 end
